612ea2963a7ba637c99548cf152a7ba6c75f997c
Chethan.Gangoor <chethansgangoor@excelindia.com>
2014-07-24 10:13:50 +0530

    Update the commonstep (R)

--- v2/Pegasus.Acceptance.MyItLab.Tests/CommonProductAcceptanceTestDefinitions/CommonSteps.cs
+++ v2/Pegasus.Acceptance.MyItLab.Tests/CommonProductAcceptanceTestDefinitions/CommonSteps.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using Pegasus.Pages.Exceptions;
+using Pegasus.Automation.DataTransferObjects;
 using Pearson.Pegasus.TestAutomation.Frameworks;
 using Pearson.Pegasus.TestAutomation.Frameworks.DataTransferObjects;
 using Pegasus.Pages.UI_Pages;
