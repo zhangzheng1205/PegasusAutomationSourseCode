c0985a4016ecc834603c9ed6829bb9dd0eaf69c9
Sheo Singh <sheo.singh@imfinity.com>
2014-09-23 14:43:56 +0530

    PEGASUS-30344 Automation : HED BVT : Sim 5 Excel activity and Student scoring a 100%

---
+++ v2/Pegasus.Pages/UI Pages/Integration/SIM5/SIM5Services/SIM5FramePage.cs
@@ -0,0 +1,12 @@
+﻿using Pearson.Pegasus.TestAutomation.Frameworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pegasus.Pages.UI_Pages
+{
+    public class SIM5FramePage:BasePage
+    {
+    }
+}
