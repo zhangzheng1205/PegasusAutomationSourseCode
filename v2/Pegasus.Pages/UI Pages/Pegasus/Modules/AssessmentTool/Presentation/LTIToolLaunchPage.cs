edfa3a1c9e2694095311334d171e7d605ca03456
rashmi shetty <rashmi.shetty@excelindia.com>
2015-02-06 17:27:08 +0530

    Added missed cs file to fix Build Error(rashmi)

---
+++ v2/Pegasus.Pages/UI Pages/Pegasus/Modules/AssessmentTool/Presentation/LTIToolLaunchPage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Pearson.Pegasus.TestAutomation.Frameworks;
+using Pegasus.Pages.Exceptions;
+
+namespace Pegasus.Pages.UI_Pages
+{
+    /// <summary>
+    /// This class handles the LTIToolLaunch Page Ui Actions.
+    /// </summary>
+    public class LTIToolLaunchPage : BasePage
+    {
+        /// <summary>
+        /// The static instance of the logger for the class.
+        /// </summary>
+        private static readonly Logger Logger = Logger.GetInstance(typeof(LTIToolLaunchPage));
+
+        /// <summary>
+        /// Get The Presentation Page Window Title.
+        /// </summary>
+        /// <param name="presentationPageTitle">This is Presentation Page Title.</param>
+        /// <returns>Selected Window Title.</returns>
+        public string GetPresentationPageWindowTitle(string presentationPageTitle)
+        {
+            Logger.LogMethodEntry("LTIToolLaunchPage",
+              "GetPresentationPageWindowTitle", base.IsTakeScreenShotDuringEntryExit);
+            string selectedWindowTitle = string.Empty;
+            try
+            {
+                this.SelectPresentationPage(presentationPageTitle);
+                selectedWindowTitle = base.GetPageTitle;
+            }
+            catch (Exception e)
+            {
+                ExceptionHandler.HandleException(e);
+            }
+            Logger.LogMethodExit("LTIToolLaunchPage",
+              "GetPresentationPageWindowTitle", base.IsTakeScreenShotDuringEntryExit);
+            return selectedWindowTitle;
+        }
+
+        /// <summary>
+        /// Select Presentation Page.
+        /// </summary>
+        /// <param name="presentationPageTitle">This is Presentation Page Name.</param>
+        private void SelectPresentationPage(string presentationPageTitle)
+        {
+            Logger.LogMethodExit("LTIToolLaunchPage",
+                "SelectPresentationPage", base.IsTakeScreenShotDuringEntryExit);
+            base.WaitUntilWindowLoads(presentationPageTitle);
+            base.SelectWindow(presentationPageTitle);
+            Logger.LogMethodExit("LTIToolLaunchPage",
+                "SelectPresentationPage", base.IsTakeScreenShotDuringEntryExit);
+        }
+    }
+}
